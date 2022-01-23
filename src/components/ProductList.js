29531586ea15e9ea3bc49a3ca1e1b8a82b22d195
achraf-ayadi <ayadi.achraf2@gmail.com>
2022-01-23 16:49:22 +0100

    adding filterContext

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -4,7 +4,18 @@ import GridView from './GridView'
 import ListView from './ListView'
 
 const ProductList = () => {
-  return <h4>product list</h4>
+  const { filtredProducts, gridView } = useFilterContext()
+  if (filtredProducts.length < 1) {
+    return (
+      <h5 style={{ textTransform: 'none' }}>
+        sorry, no products matched your search
+      </h5>
+    )
+  }
+  if (gridView) {
+    return <GridView products={filtredProducts} />
+  }
+  return <ListView products={filtredProducts} />
 }
 
 export default ProductList
